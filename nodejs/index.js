371908cc8197b717d6a97c04024a97cfdb6d3dd3
yuandadu <duyuanda@yeah.net>
2018-02-11 19:00:29 -0800

    build the front websites

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,47 +1,48 @@
-// -get command line argument
-
-var argv = require('minimist')(process.argv.slice(2))
-
+// - get command line arguments
+var argv = require('minimist')(process.argv.slice(2));
+var port = argv['port'];
 var redis_host = argv['redis_host'];
 var redis_port = argv['redis_port'];
-var redis_channel = argv['redis_channel'];
+var subscribe_topic = argv['subscribe_topic'];
 
+// - setup dependency instances
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-// create scoket io server
-
-var scoketio = require('socket.io')(server);
-
-
-// setup redis client
-
+// - setup redis client
 var redis = require('redis');
-console.log('create redis client');
-
-var redisClient = redis.createClient(redis_port, redis_host);
-console.log('subcribe to redis topic %s', redis_channel);
-
-redisClient.subscribe(redis_channel);
-
-// register redis callback function
-
-redisClient.on('message', function(channel, message) {
-	if(channel == redis_channel) {
-		console.log('message received %s', message);
-		// define event called data
-		scoketio.sockets.emit('data', message);
-	}
+console.log('Creating a redis client');
+var redisclient = redis.createClient(redis_port, redis_host);
+console.log('Subscribing to redis topic %s', subscribe_topic);
+redisclient.subscribe(subscribe_topic);
+redisclient.on('message', function (channel, message) {
+    if (channel == subscribe_topic) {
+        console.log('message received %s', message);
+        io.sockets.emit('data', message);
+    }
 });
 
-app.use(express.static(__dirname = './public'));
-app.use('/jquery', express.static(__dirname = './node_modules/jquery/dist/'));
-app.use('/bootstrap', express.static(__dirname = './node_modules/bootstrap/dist/'));
-app.use('/d3', express.static(__dirname = './node_modules/d3/'));
-app.use('/nvd3', express.static(__dirname = './node_modules/nvd3/build'));
+// - setup webapp routing
+app.use(express.static(__dirname + '/public'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/d3', express.static(__dirname + '/node_modules/d3/build/'));
+app.use('/nvd3', express.static(__dirname + '/node_modules/nvd3/build/'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 
-
-server.listen(3000, function() {
-	console.log('server started at port 3000')
+server.listen(port, function () {
+    console.log('Server started at port %d.', port);
 });
+
+// - setup shutdown hooks
+var shutdown_hook = function () {
+    console.log('Quitting redis client');
+    redisclient.quit();
+    console.log('Shutting down app');
+    process.exit();
+};
+
+process.on('SIGTERM', shutdown_hook);
+process.on('SIGINT', shutdown_hook);
+process.on('exit', shutdown_hook);
